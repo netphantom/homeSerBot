cb6d9c7162bb3169502f73af0b8aa10cdb86ea9f
net_phantom <netphantom@gmx.com>
2021-03-09 13:20:00 +0100

    Edited server IP addres

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	tApi := flag.String("tApi", "<Telegram Token>", "The Telegram API token")
 
 	addr := flag.String("addr", "4000", "The network port to use")
-	ip := fmt.Sprintf("127.0.0.1:%s", *addr)
+	ip := fmt.Sprintf("0.0.0.0:%s", *addr)
 
 	dbUserName := flag.String("dbUserName", "admin", "The database username")
 	dbPass := flag.String("dbPass", "admin", "The database password")
